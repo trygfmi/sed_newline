b8b847aec0802391e6a553abf415d2a2675d9656
running-terminal-commands <runningterminalcommands@gmail.com>
2025-12-05 02:09:11 +0900

    開発用リポジトリにfirst commit

---
+++ command_example.sh
@@ -0,0 +1,4 @@
+# 以下のようにi\の後は改行する必要がある
+sed -i '' 99'i\
+hello\                 
+' readme_test6.md
---
+++ test/macos/execute_procedure_code.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# ./execute_procedure_code.sh
+
+
+echo "execute_procedure_code.sh"
+
+
+shopt -s expand_aliases
+file_absolute_path=$(readlink -f "$0")
+parent_directory_path=$(dirname "$file_absolute_path")
+repository=$(dirname "$(dirname "$parent_directory_path")")
+repository_name=$(basename "$(dirname "$(dirname "$parent_directory_path")")")
+test_os_path=$(basename "$(dirname "$parent_directory_path")")"/"$(basename "$parent_directory_path")"/"
+now=$(date +%Y-%m-%d_%H:%M:%S)
+
+output_procedure_path="$parent_directory_path""/compare-auto/auto_procedure_output"$now".txt"
+read_procedure_file_path="get_article_procedure_code/"$(ls -1t "$test_os_path"get_article_procedure_code | sed -n '1p')
+
+cd "$parent_directory_path"
+#####################################################
+rm -rf "$repository""/""$test_os_path""$repository_name"
+#####################################################
+
+i=0
+sed_command_string1=""
+sed_command_string2=""
+sed_command_string3=""
+while IFS= read -r command; do
+    echo "$command"
+
+    if [ $i == 4 ]; then
+        sed_command_string1="$command"
+    elif [ $i == 5 ]; then
+        sed_command_string2="$command"
+    elif [ $i == 6 ]; then
+        sed_command_string3="$command"
+        bash -c "$sed_command_string1
+$sed_command_string2
+$command"
+    else
+        eval "$command"
+    fi
+
+    i=$(($i+1))
+done < "$read_procedure_file_path"
+
+cp hello_macos.txt $output_procedure_path
+
+
